8a87585ca6e9999c69212d4841608f0e894b862e
Vishwanathpd <vishwanthdalawai321@gmail.com>
2020-05-02 16:59:56 +0530

    Updates

---
+++ precache-manifest.6bee7ebc4867f7aff2d4852ece1957d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd553d2bd7986863276200fc393e6ce5",
+    "url": "/VishwanathDalawai/simple-todo-app/index.html"
+  },
+  {
+    "revision": "c2be5c7e83fb23b5d988",
+    "url": "/VishwanathDalawai/simple-todo-app/static/css/main.f66a9211.chunk.css"
+  },
+  {
+    "revision": "81309eddfefb405792e3",
+    "url": "/VishwanathDalawai/simple-todo-app/static/js/2.e9a03e88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/VishwanathDalawai/simple-todo-app/static/js/2.e9a03e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2be5c7e83fb23b5d988",
+    "url": "/VishwanathDalawai/simple-todo-app/static/js/main.3df6a14f.chunk.js"
+  },
+  {
+    "revision": "d8ca3e472adb24fdbbd6",
+    "url": "/VishwanathDalawai/simple-todo-app/static/js/runtime-main.a441c96d.js"
+  }
+]);
